3fb3f83fe173fb57690389d0eec64852ec9ee164
Raja0605 <145437896+Raja0605@users.noreply.github.com>
2024-08-22 11:08:55 -0700

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,16 @@
+var productContainer = document.getElementById("products");
+var search = document.getElementById("search");
+var productlist = productContainer.querySelectorAll("div");
+
+search.addEventListener("keyup", function(event) {
+    var enteredValue = event.target.value.toUpperCase();
+
+    for (var count = 0; count < productlist.length; count++) {
+        var productname = productlist[count].querySelector("p").textContent;
+        if (productname.toUpperCase().indexOf(enteredValue) < 0) {
+            productlist[count].style.display = "none";
+        } else {
+            productlist[count].style.display = "block";
+        }
+    }
+});
---
+++ nostra.js
@@ -0,0 +1,9 @@
+
+var sidenavbar=document.querySelector(".side-navbar")
+  
+function shownavbar(){
+    sidenavbar.style.left="0%"
+}
+function closenavbar(){
+    sidenavbar.style.left="-60%"
+}
